4124c6a0a99aa6e61201189f46dae0fe8a5153fe
Sahil Gupta <153733143+sg-740@users.noreply.github.com>
2024-05-31 23:53:56 +0530

    Add files via upload

---
+++ Appointment Scheduler Project.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Appointment Scheduler Project.aspx.cs" Inherits="ConnectSQL.Appointment_Scheduler_Project" %>
+
+<%@ Register Assembly="MSCaptcha" Namespace="MSCaptcha" TagPrefix="cap" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Appointment Scheduler Form</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+        }
+
+        .container {
+            max-width: 500px;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .form-control {
+            border-radius: 20px;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+
+        .error-message {
+            color: red;
+        }
+
+        .auto-style1 {
+            width: 162px;
+        }
+
+        .text-blue {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2 class="mb-4 text-blue"><b>Appointment Scheduler Form</b></h2>
+            <div class="form-group">
+                <label for="lblName">Username:</label>
+                <asp:TextBox ID="txtName" runat="server" CssClass="form-control" placeholder="Enter Your Name"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="lblMobile">Mobile No:</label>
+                <asp:TextBox ID="txtMobile" runat="server" CssClass="form-control" placeholder="Enter Your Mobile No" TextMode="Number"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="lblEmail">Email ID:</label>
+                <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" placeholder="Example1234@gmail.com" TextMode="Email"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="lblPassword">Password:</label>
+                <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" placeholder="Enter Your Password" TextMode="Password"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="lblAddress">Address:</label>
+                <asp:TextBox ID="txtAddress" runat="server" CssClass="form-control" placeholder="Enter Your Address" TextMode="MultiLine"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label for="lblGender">Select Your Gender:</label>
+                <asp:DropDownList ID="DropDownList1" runat="server" CssClass="form-control">
+                    <asp:ListItem Value="Male">Male</asp:ListItem>
+                    <asp:ListItem Value="Female">Female</asp:ListItem>
+                    <asp:ListItem Value="TransGender">TransGender</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+            <div class="form-group">
+                <label for="lblTime">Timing:</label>
+                <asp:TextBox ID="txtTime" runat="server" CssClass="form-control" type="datetime-local"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <table class="w-100">
+                    <tr>
+                        <td>
+                            <cap:CaptchaControl ID="captcha1" runat="server" CaptchaLength="5" CaptchaHeight="50" CaptchaWidth="200" CaptchaLineNoise="None" CaptchaMinTimeout="3" CaptchaMaxTimeout="240" CaptchaChars="abcdefghijklmnopqrstuvwxyz123456789" ForeColor="Blue" BackColor="Yellow" Height="46px" Width="200px" />
+                            <asp:TextBox ID="txtCaptcha" runat="server" placeholder="Enter Captcha" CssClass="form-control"></asp:TextBox>
+                        </td>
+                        <td>
+                            <asp:Button ID="btnRefresh" runat="server" Text="Refresh" CssClass="btn btn-primary mb-3" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn btn-primary mb-3" OnClick="btnSave_Click" />
+            <br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Appointment Scheduler Project.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ConnectSQL
+{
+    public partial class Appointment_Scheduler_Project : System.Web.UI.Page
+    {
+        //SqlConnection con = new SqlConnection("Data Source=SAHIL\\SQLEXPRESS; Initial Catalog=Appointment;Integrated Security=True;");
+        string dbconnection = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtName.Focus();
+            }
+        }   
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            captcha1.ValidateCaptcha(txtCaptcha.Text.Trim());
+            if (captcha1.UserValidated)
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                lblMessage.Text = "Your Captcha Text Has Been Verified Successfully ";
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Your Captcha Text Has Not Verified Successfully! ";
+            }
+            bool validate = ValidateForm();
+            if (validate)
+            {
+                SqlConnection con = new SqlConnection(dbconnection);
+                SqlCommand cmd = new SqlCommand("sp_table", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@mobile", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+                cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@gender", DropDownList1.Text);
+                cmd.Parameters.AddWithValue("@timing", txtTime.Text);
+                con.Open();
+                int RowAffected = cmd.ExecuteNonQuery();
+                if (RowAffected != 6)
+                {
+                    lblMessage.Text = "Record Inserted Successfully into the Database";
+                }
+                else
+                {
+                    lblMessage.Text = "Record Not inserted";
+                }
+                con.Close();
+            }
+        }
+
+            public bool ValidateForm()
+            {
+                if (string.IsNullOrEmpty(txtName.Text.ToString()) && string.IsNullOrEmpty(txtEmail.Text.ToString()) && string.IsNullOrEmpty(txtCaptcha.Text.ToString()) && string.IsNullOrEmpty(txtAddress.Text.ToString()) && string.IsNullOrEmpty(txtMobile.Text.ToString()) && string.IsNullOrEmpty(txtPassword.Text.ToString()) && string.IsNullOrEmpty(txtTime.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter all the fields')</script>";
+                    return false;
+                }
+                if (string.IsNullOrEmpty(txtName.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Name')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtEmail.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Email Id')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtCaptcha.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Captcha')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtAddress.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Address')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtMobile.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Mobile')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtPassword.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Password')</script>";
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(txtTime.Text.ToString()))
+                {
+                    lblMessage.Text = "<script>alert('Please Enter Time')</script>";
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+        
+        }
+    
+}
